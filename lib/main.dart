8ee7f90f003ebb9e0e7e7c9a721813d4134c2732
RahulMore-Codes <dev.rahulmore@gmail.com>
2021-08-30 15:25:17 +0530

    Added Text widget when there is no task

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:to_doof/src/model/task_data.dart';
 import 'package:to_doof/src/screens/tasks_screen.dart';
 
 void main() {
-  //Make app view Portrait only
+  //Making app view portrait only
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
--- lib/src/widgets/tasks_list.dart
+++ lib/src/widgets/tasks_list.dart
@@ -8,51 +8,63 @@ class TasksList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
       builder: (context, taskData, child) {
-        return ListView.builder(
-          padding: EdgeInsets.only(bottom: 80.0),
-          itemCount: taskData.taskCount,
-          itemBuilder: (context, index) {
-            final task = taskData.tasks[index];
-            return TaskTile(
-              taskTitle: task.taskName,
-              isChecked: task.isDone,
-              checkBoxState: (state) => taskData.updateTask(task),
-              onLongPressCallback: () => showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text("Delete Task?"),
-                  content: Text(task.taskName),
-                  actions: [
-                    TextButton(
-                      child: Text(
-                        "Cancel",
-                        style: TextStyle(color: Theme.of(context).primaryColor),
+        return (taskData.tasks.length > 0)
+            ? ListView.builder(
+                padding: EdgeInsets.only(bottom: 80.0),
+                itemCount: taskData.taskCount,
+                itemBuilder: (context, index) {
+                  final task = taskData.tasks[index];
+                  return TaskTile(
+                    taskTitle: task.taskName,
+                    isChecked: task.isDone,
+                    checkBoxState: (state) => taskData.updateTask(task),
+                    onLongPressCallback: () => showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text("Delete Task?"),
+                        content: Text(task.taskName),
+                        actions: [
+                          TextButton(
+                            child: Text(
+                              "Cancel",
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor),
+                            ),
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                            ),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          TextButton(
+                            child: Text(
+                              "Delete",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                            ),
+                            onPressed: () {
+                              //delete the task
+                              taskData.deleteTask(task);
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
                       ),
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                      ),
-                      onPressed: () => Navigator.pop(context),
                     ),
-                    TextButton(
-                      child: Text(
-                        "Delete",
-                        style: TextStyle(color: Colors.red),
-                      ),
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                      ),
-                      onPressed: () {
-                        //delete the task
-                        taskData.deleteTask(task);
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
+                  );
+                },
+              )
+            : Center(
+                child: Text(
+                  "Add Tasks",
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-            );
-          },
-        );
+              );
       },
     );
   }
